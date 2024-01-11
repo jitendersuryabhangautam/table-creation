c059fd510fd8f058897792f2de7964ab833f1934
jitendersuryabhangautam <jitendergoswami.835@gmail.com>
2024-01-12 00:19:41 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import BasicTable from './components/BasicTable';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hi this is me jitender</h1>
+     <BasicTable/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/columns.js
@@ -0,0 +1,32 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        Footer: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'First Name',
+        Footer: 'First Name',
+        accessor: 'first_name'
+    },
+    {
+        Header: 'Last Name',
+        Footer: 'Last Name',
+        accessor: 'last_name'
+    },
+    {
+        Header: 'Date of Birth',
+        footer: 'Date of Birth',
+        accessor: 'date_of_birth'
+    },
+    {
+        Header: 'Countary',
+        Footer: 'Countary',
+        accessor: 'countary'
+    },
+    {
+        Header: 'Phone',
+        Footer: 'Phone',
+        accessor: 'phone'
+    }
+]
